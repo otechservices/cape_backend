771e481046014c4fc8f8a6a4ddbe2653e9258e6d
Ornice <ornihouss1@gmail.com>
2025-10-27 18:32:31 +0100

    fix data

--- database/migrations/2025_10_19_154901_add_fields_to_residents_table.php
+++ database/migrations/2025_10_19_154901_add_fields_to_residents_table.php
@@ -12,6 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('residents', function (Blueprint $table) {
+                $table->dropForeign('cape_id');
+
                 $table->dropColumn('cape_id');
 
                  $table->unsignedBigInteger('promoter_id')->nullable();
