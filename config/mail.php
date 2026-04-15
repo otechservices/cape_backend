6703e6076d8e2d749529af3045ae68386a64c8f9
Otech Services <otechservices24@gmail.com>
2026-04-15 10:44:43 +0000

    last correction

--- config/mail.php
+++ config/mail.php
@@ -46,6 +46,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
+	'brevo' => [
+        'transport' => 'brevo',
+    ],
+
         'ses' => [
             'transport' => 'ses',
         ],
